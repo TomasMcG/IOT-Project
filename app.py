50a5552f3e055f97e086977bd56cec2daf3bf458
Tomas <tomasmcgrath21@gmail.com>
2025-05-03 15:27:03 +0100

    Fix azure issues and connect for relay light response.

--- app.py
+++ app.py
@@ -12,12 +12,18 @@ from keys import *
 
 connection_string = connection_string
 device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+id = "a5046072-6943-4d0c-a294-1aa93032e545"
+server_command_topic = id + '/commands'
 
+client_name = id + 'soilmoisturesensor_client'
+client_telemetry_topic = id + '/telemetry'
 
 print('Connecting')
 device_client.connect()
 print('Connected')
 
+relay = GPIO(12,GPIO.OUT)
+relay = GroveRelay(5)
 
 def handle_method_request(request):
     print("Direct method received - ", request.name)
@@ -30,8 +36,9 @@ def handle_method_request(request):
     method_response = MethodResponse.create_from_method_request(request, 200)
     device_client.send_method_response(method_response)
 
-device_client.on_method_request_received = handle_method_request
+
 #tells iot hub client to call handle method request function when direct method is called
+device_client.on_method_request_received = handle_method_request
 
 
 
@@ -43,24 +50,24 @@ class GroveLightSensor:
 
     def read_light(self):
         return self.adc.read(self.channel)
+    
+
 
 def main():
     pin = 2  
     sensor = GroveLightSensor(pin)
-
-
     camera = PiCamera()
     camera.resolution = (640, 480)
     camera.rotation = 180
-
-    time.sleep(2)
-
- 
-
-    relay = GPIO(12,GPIO.OUT)
-    relay = GroveRelay(5)
-
+    image = io.BytesIO()
+    camera.capture(image, 'jpeg')
+    image.seek(0)
+    with open('image.jpg', 'wb') as image_file:
+        print('Taking Photo')
+        image_file.write(image.read())
+    camera.close()
     print(f"Testing Grove Light Sensor on pin A{pin}...")
+
     while True:
         light_value = sensor.read_light()
         print(f"Light Sensor Value: {light_value}")
@@ -69,17 +76,7 @@ def main():
         message = Message(json.dumps({ 'light_value': light_value }))
         device_client.send_message(message)
         time.sleep(5) 
-        time.sleep(1)
-        if light_value > 100:
-            relay.on()
-            image = io.BytesIO()
-            camera.capture(image, 'jpeg')
-            image.seek(0)
-            with open('image.jpg', 'wb') as image_file:
-                print('Taking Photo')
-                image_file.write(image.read())
-        elif light_value <= 100:
-            relay.off()
+
  
 
   
